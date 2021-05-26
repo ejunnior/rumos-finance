91ebbcf8d398e29eec60a6249448f13360df9cda
Edvaldo Junior <ejunnior@gmail.comgit config --global user.email ejunnior@gmail.comgit config --global user.email ejunnior@gmail.com>
2021-05-26 21:07:59 +0100

    Adding console application to connect to the infra data

---
+++ service/src/TestRepo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestRepo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite o nome da Categoria:");
+
+            var input = Console.ReadLine();
+
+            //Aplicar o dominio
+
+            //Chamar o repositorio
+        }
+    }
+}
