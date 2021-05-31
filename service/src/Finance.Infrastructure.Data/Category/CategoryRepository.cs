3243cba249a226166516c67c4476e92900e11b14
Edvaldo Junior <ejunnior@gmail.comgit config --global user.email ejunnior@gmail.comgit config --global user.email ejunnior@gmail.com>
2021-05-31 21:42:06 +0100

    .

---
+++ service/src/Finance.Infrastructure.Data/Category/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Finance.Infrastructure.Data.Category
+{
+    using Domain.Category.Aggregates.CategoryAggregate;
+    using UnitOfWork;
+
+    public class CategoryRepository : Repository<Category>
+    {
+        public CategoryRepository(FinanceUnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+    }
+}
