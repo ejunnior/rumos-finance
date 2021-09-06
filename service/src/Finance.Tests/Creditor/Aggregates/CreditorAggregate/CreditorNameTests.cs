14683b95485d04596002698dfe9c4185bf4714ac
Edvaldo Junior <EJUNIOR@volvocars.com>
2021-09-06 22:19:04 +0200

    adding unit tests

---
+++ service/src/Finance.Tests/Creditor/Aggregates/CreditorAggregate/CreditorNameTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Finance.Tests.Creditor.Aggregates.CreditorAggregate
+{
+    using Finance.Domain.Creditor.Aggregates.CreditorAggregate;
+    using Xunit;
+
+    public class CreditorNameTests
+    {
+        [Fact]
+        public void ShouldCreditorNameBeCreated()
+        {
+            // Arrange
+            var expectedName = "Creditor Name";
+
+            // Act
+            var sut = CreditorName
+                .Create(expectedName);
+
+            // Assert
+            Assert.Equal(expectedName, sut.Value.Value);
+        }
+
+        [Fact]
+        public void ShouldCreditorNameNotBeCreated()
+        {
+            // Arrange
+            var expectedName = string.Empty;
+
+            // Act
+            var sut = CreditorName
+                .Create(expectedName);
+
+            // Assert
+            Assert.True(sut.IsFailure);
+            Assert.Equal("Value is Required", sut.Error);
+        }
+    }
+}
