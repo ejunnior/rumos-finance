7bb48c3a2603d750262bb8313663532e5bec6bb5
Edvaldo Junior <EJUNIOR@volvocars.com>
2021-09-07 22:03:55 +0200

    E2E test

---
+++ service/src/Finance.Tests.UI/PageObject/HomePage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Finance.Tests.UI.PageObject
+{
+    using OpenQA.Selenium;
+
+    public class HomePage
+    {
+        private readonly IWebDriver _webDriver;
+        private const string PageUrl = "http://localhost:4200/";
+
+        public HomePage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public void NavigateTo()
+        {
+            _webDriver.Navigate().GoToUrl(PageUrl);
+        }
+
+        public CreditorListPage ClickCreditorLink()
+        {
+            _webDriver.FindElement(By.Id("creditor")).Click();
+            return new CreditorListPage(_webDriver);
+        }
+    }
+}
